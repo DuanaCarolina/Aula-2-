9c011614bcf113e4f8d118a8b897c14340008634
TEC. WEB <duana_ana@yahoo.com.br>
2021-09-06 17:19:35 -0300

    Aula node

---
+++ Aula Node/Aula2.js
@@ -0,0 +1,24 @@
+nome = "Gabriela"
+idade = 20
+telefone = 41845542
+endereco = "Rua Maria Rosa"
+numerodacasa = 55
+cidade = "Itapevi"
+cep = 1079010
+CPF= 39551475819
+RG= 46893999
+
+   
+
+
+console.log("o nome do contato é": + nome + "ele tem idade" + idade + "anos de idade, o numero de contato dele é + tel+ mora no endereço" + n + da residencia na cidade de"+");
+console.log("numero do CPF:" + CPF + "RG: " = rg)
+
+
+
+
+
+
+
+
+
